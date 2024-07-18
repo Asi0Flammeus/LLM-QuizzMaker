3689dd5f86d1ca46dca9409ab23daebd223a2483
Asi0Flammeus <asi0.flammeus@pm.me>
2024-07-18 10:52:42 +0200

    add todo for test

--- course.py
+++ course.py
@@ -73,18 +73,16 @@ class Course:
     def get_current_chapter_index(self):
         return self.current_chapter['chapter_index']
 
-"""
-test zone --> I should do that properly in test_course.py
-path = "./inputs/test/"
-with open(os.path.join(path, 'en.md'), 'r') as file:
-    raw_course = file.read()
-
-course = Course(raw_course)
-
-# Extract the text of Chapter 2 from Section 2
-chapter_text = course.get_chapter_text(2, 2)
-if chapter_text:
-    print(chapter_text)
-else:
-    print("Chapter not found!")
-"""
+# TODO: test zone --> I should do that properly in test_course.py
+# path = "./inputs/test/"
+# with open(os.path.join(path, 'en.md'), 'r') as file:
+#     raw_course = file.read()
+#
+# course = Course(raw_course)
+#
+# # Extract the text of Chapter 2 from Section 2
+# chapter_text = course.get_chapter_text(2, 2)
+# if chapter_text:
+#     print(chapter_text)
+# else:
+    # print("Chapter not found!")
